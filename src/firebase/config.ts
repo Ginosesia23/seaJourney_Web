f007ca5814c4d34817e5886352d5bd152a8c5f38
Gino Sesia <ginosesia23@gmail.com>
2025-11-03 19:15:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5088907108-439c0",
+  "appId": "1:452958380575:web:a262fcd05bc3ada537eb2a",
+  "apiKey": "AIzaSyAF_CzV0bqUef2Wnwv_axtE1WBKfC6mbkk",
+  "authDomain": "studio-5088907108-439c0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "452958380575"
+};
