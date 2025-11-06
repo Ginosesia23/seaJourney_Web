574d14fa6eff9706407d4ea936b4d2cd15828925
Gino Sesia <ginosesia23@gmail.com>
2025-11-06 18:59:01 +0000

    it  is still the dark background, only the nav bar should have the dark

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -36,7 +36,7 @@ export default function DashboardLayout({
   return (
     <div className="theme-dashboard flex min-h-screen w-full flex-col">
       <DashboardHeader />
-      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+      <main className="flex flex-1 flex-col gap-4 bg-background p-4 md:gap-8 md:p-8">
         {children}
       </main>
     </div>
