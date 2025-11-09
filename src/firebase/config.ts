09633ee5190f4abd96653c90397ac309ed80438c
Gino Sesia <ginosesia23@gmail.com>
2025-11-09 08:23:47 +0000

    AIzaSyD5WDrENZF2cpM2bpRZRQlcyP30jV56nl0

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "seajourney-b9cbc",
   "appId": "1:396973682729:web:35520108ad1bf4479b182b",
-  "apiKey": "AIzaSyDEjR6lVwE1jP1o_s-mK8r9Z8j_F0v_YwE",
+  "apiKey": "AIzaSyD5WDrENZF2cpM2bpRZRQlcyP30jV56nl0",
   "authDomain": "seajourney-b9cbc.firebaseapp.com",
   "measurementId": "G-L5G8H2V6T5",
   "storageBucket": "seajourney-b9cbc.appspot.com",
