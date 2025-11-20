94e3297af2170230c619acc4458909a6413c2178
Gino Sesia <ginosesia23@gmail.com>
2025-11-20 19:24:12 +0000

    Try fixing this error: `Runtime Error: Error: useRevenueCat must be used

---
+++ src/app/coming-soon/layout.tsx
@@ -0,0 +1,9 @@
+import RevenueCatProvider from '@/components/providers/revenue-cat-provider';
+
+export default function ComingSoonLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <RevenueCatProvider>{children}</RevenueCatProvider>;
+}
