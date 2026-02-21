cb31e7dd56fd27df9747cbfb1bf28216e8437f3d
Ginosesia23 <ginosesia23@gmail.com>
2026-02-21 19:35:58 +0100

    new icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="light scroll-smooth" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/icon.png" type="image/png" sizes="any" />
+        <link rel="apple-touch-icon" href="/icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
